9697d04ec13001569c1d069c03df887918b5f90b
Shravan Bhati <153543313+shravanbhati@users.noreply.github.com>
2024-04-13 21:33:50 +0530

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -71,7 +71,7 @@ document.addEventListener('visibilitychange',
 
 // <!-- typed js effect starts -->
 var typed = new Typed(".typing-text", {
-    strings: ["frontend development", "backend development", "web development","Game development","BOT development","Moderation"],
+    strings: ["frontend development", "backend development", "web development","Game development","BOT development"],
     loop: true,
     typeSpeed: 50,
     backSpeed: 25,
